7ba8f2aa6ac6de7a39930cf24ec2c9d6ba0677c0
Vincent Rios <riverman@Vincents-MBP-2.fios-router.home>
2019-02-01 14:45:34 -0500

    add react-router to the repo

--- client/src/App.js
+++ client/src/App.js
@@ -30,8 +30,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Service Worker - POC</h1>
         </header>
         {/* Render the newly fetched data inside of this.state.data  */}
         <p className="App-intro">{this.state.data}</p>
